3e58902c8c1035e3322d163781ddeca8ab092d7d
Tun Cham Roeun <chamroeuntun25@outlook.com>
2024-02-27 20:32:27 +0700

    build:dist

--- src/multiselectMixin.js
+++ src/multiselectMixin.js
@@ -507,7 +507,7 @@ export default {
      * @param  {Boolean} block removing
      */
     select (option, key) {
-      console.log(option, 'hello this is me')
+      console.log(option, 'option updated')
       /* istanbul ignore else */
       if (option.$isLabel && this.groupSelect) {
         this.selectGroup(option)
