90bfd56c413de7501b917be7b55953bdca68c669
Tun Cham Roeun <chamroeuntun25@outlook.com>
2024-02-27 21:18:57 +0700

    support internal  value

--- src/multiselectMixin.js
+++ src/multiselectMixin.js
@@ -350,6 +350,11 @@ export default {
   },
   computed: {
     internalValue () {
+      if (this.field && this.field.value) {
+        return this.value || this.value === 0
+          ? Array.isArray(this.value) ? this.value.map(val => this.options.find(option => option[this.field.value] === val)) : this.options.find(option => option[this.field.value] === this.value)
+          : []
+      }
       return this.value || this.value === 0
         ? Array.isArray(this.value) ? this.value : [this.value]
         : []
